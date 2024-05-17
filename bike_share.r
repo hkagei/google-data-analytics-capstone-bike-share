bba30feeb7ba04d68a83dcb47e21a6bb16118578
Hiroyuki "Hiro" Kagei <k_dawg_1022@hotmail.com>
2024-05-16 19:14:31 -0600

    Update bike_share.r

--- bike_share.r
+++ bike_share.r
@@ -1,1 +1,28 @@
+#Loading libraries
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("ggplot2")
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
 
+#Loading .csv files
+q1_2019 <- read_csv("Divvy_Trips_2019_Q1.csv")
+q1_2020 <- read_csv("Divvy_Trips_2020_Q1.csv")
+
+#Compare column names each of the files
+colnames(q1_2019)
+colnames(q1_2020)
+
+#Rename columns to make them consistent with q1_2020
+(q1_2019 <- rename(q1_2019
+                   ,ride_id = trip_id
+                   ,rideable_type = bikeid
+                   ,started_at = start_time
+                   ,ended_at = end_time
+                   ,start_station_name = from_station_name
+                   ,start_station_id = from_station_id
+                   ,end_station_name = to_station_name
+                   ,end_station_id = to_station_id
+                   ,member_casual = usertype
+                   ))
